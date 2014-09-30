f4f0714ebd29755e43fe1a9ba62404b21de179dc
Benjamin Fortune <benjamin.fortune1@gmail.com>
2014-09-30 09:40:22 +0100

    Allow IP:PORT format for host, better players method for iw4.

--- examples/server.js
+++ examples/server.js
@@ -2,15 +2,13 @@ var reQ = require('../lib');
 
 reQ.query({
     type: 'iw4',
-    host: '37.187.161.194',
-    port: 28985,
+    host: '174.2.136.143:28961',
     timeout: 1500,
     parse: true
 }).then(function(state) {
     reQ.query({
         type: 'iw4/players',
-        host: '37.187.161.194',
-        port: 28985,
+        host: '174.2.136.143:28961',
         parse: true
     }).then(function(players) {
         state.players = players;
